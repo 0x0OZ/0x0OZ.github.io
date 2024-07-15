41be342ea8f921faa1a1ad510252150097531a80
0xOZ <iiahmad435@gmail.com>
2024-07-15 05:05:06 -0400

    moved to custom build & disabled gh deployments

---
+++ root/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "MetaCTF v5 2024 Writeups",
+        "excerpt":"Writeups for the MetaCTF v5 2024 CTF Web Challenges that was held on 30th June 2024. Python’s Twister Challenge Description Hack our admin. Solution The challenge is a simple flask application that has register/login/pass-reset functionalities. The app generates 10000 random numbers and assigns one to each user when registered as...","categories": [],
+        "tags": [],
+        "url": "/writeups/metactf-v5-2024",
+        "teaser": null
+      },{
+        "title": "NCSC Training 2024 Writeups",
+        "excerpt":"Those are writeups for the challenges of the NCSC Training 2024 CTF, the CTF was held on 11th July 2024 to 13th July 2024. Web Robot Challenge Description A Web crawler, sometimes called a robot, is an Internet bot that systematically browses the World Wide Web and that is typically...","categories": [],
+        "tags": [],
+        "url": "/writeups/ncsc-training-2024",
+        "teaser": null
+      },]
