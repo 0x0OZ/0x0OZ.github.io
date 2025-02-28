78dcc6beb3814ba942a6ce11ee150cb90dac9eef
0xOZ <abuzaid.sahab.2015@gmail.com>
2025-03-01 01:11:25 +0300

    fixed baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "0x0oz.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
